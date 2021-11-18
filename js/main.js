6432ff8db4fdb213e40d416009fba15013321119
jankunasm <jankunasmatas11@gmail.com>
2021-11-17 19:03:12 -0600

    Third and Final commit.

--- js/main.js
+++ js/main.js
@@ -14,14 +14,17 @@ const getData = async (season, round) => {
     return response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings.slice(0,7)
 }
 
+
 const DOM_Elements = {
     racers : '.racer-list'
 }
+
 
 const create_list = (driverId, position, givenName, familyName, nationality, sponsor, points) => {
     const html = `<a href = '#' class = "list-group-item list-group-item-action list-group-item-light" id="${driverId}"> Position: ${position} Name: ${givenName} ${familyName} Nationality: ${nationality} Sponsor: ${sponsor}, Points: ${points}`
     document.querySelector(DOM_Elements.racers).insertAdjacentHTML('beforeend', html)
 }
+
 
 const loadData = async (jsonData) => {
     const racerList = await jsonData
